e8b75fa1d2b85c08952d412f386e23655b38d5b3
Marwen Hasnaoui <marwen.hasna@gmail.com>
2017-06-25 23:25:08 +0200

    add script and log4j-spark.properties

--- src/main/resources/script.sh
+++ src/main/resources/script.sh
@@ -1,1 +1,12 @@
-spark-submit --master spark://mbp-de-marwen.home:7077  --driver-java-options "-Dlog4j.configuration=file:///tmp/log4j.properties" --files /tmp/log4j.properties --class sparkTest.logFact.Main logFact-0.0.1-SNAPSHOT.jar
+hadoop fs -rm /user/mhasnaoui/testForMarwen/logFact-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+hadoop fs -put logFact-0.0.1-SNAPSHOT-jar-with-dependencies.jar /user/mhasnaoui/testForMarwen
+hadoop fs -rm /user/mhasnaoui/testForMarwen/log4j-spark1.properties
+hadoop fs -put log4j-spark1.properties /user/mhasnaoui/testForMarwen
+
+spark-submit --master yarn --deploy-mode cluster --conf spark.network.timeout=10000000 \
+--conf spark.executor.heartbeatInterval=10000000 --conf spark.yarn.driver.memoryOverhead=3000 --conf spark.yarn.executor.memoryOverhead=5120 --num-executors 5 --executor-cores 4 --executor-memory 10g \
+--driver-memory 5g \
+--files hdfs:///user/mhasnaoui/testForMarwen/log4j-spark1.properties \
+--conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:log4j-spark1.properties" \
+--conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:log4j-spark1.properties" \
+--class sparkTest.logFact.Main hdfs://ws44-nn1.tl.teralab-datascience.fr:8020/user/mhasnaoui/testForMarwen/logFact-0.0.1-SNAPSHOT-jar-with-dependencies.jarspark-submit --master spark://mbp-de-marwen.home:7077  --driver-java-options "-Dlog4j.configuration=file:///tmp/log4j.properties" --files /tmp/log4j.properties --class sparkTest.logFact.Main logFact-0.0.1-SNAPSHOT.jar
--- src/main/scala/sparkTest/logFact/hdfs/Collect.scala
+++ src/main/scala/sparkTest/logFact/hdfs/Collect.scala
@@ -7,26 +7,33 @@ object Collect {
   def collect(path: String) {
     
     println("start collect ====>>>>>>>>>>>>>>>>>")
-    //val log = LogManager.getLogger("myLogger")
-    //log.setLevel(Level.WARN)
+    val log = LogManager.getLogger("myLogger")
+    log.setLevel(Level.WARN)
     val hdfs = new HdfsWatcher (path, Spark.sc.hadoopConfiguration) 
-    
+    log.info("startttt collect")
+    log.warn("start collect")
+    log.error("error")
     Spark.sc.makeRDD(1 to 1000000).count()
     Spark.sc.makeRDD(1 to 1000000).count()
     Spark.sc.makeRDD(1 to 1000000).count()
     Spark.sc.makeRDD(1 to 1000000).count()
     
-    while (true) {
+    log.info("startttt collect")
+    log.warn("start collect")
+    log.error("error")
+    Spark.sc.stop()
+    
+    /*while (true) {
     
       try {
         val fileList = hdfs.getFilesPaths().map{ x => (x, x.getName) }
-        .filter { case (_ , name: String) => ! name.endsWith("#collected")}
+        .filter { case (_ , name: String) => ! name.endsWith("#collected") && ! name.endsWith("_COPYING_")}
       fileList.foreach { case (p: Path, name: String) =>
-        //log.info("start collecting "+ name)
+        log.info("start collecting "+ name)
         println(name, Spark.sc.textFile(p.toString()).count())
         //hdfs.fs.renameSnapshot(p, p.getName, "collected+"+p.getName) 
         hdfs.fs.rename(p, new Path(p.toString()+"#collected"))
-        //log.info("end collecting" + name)
+        log.info("end collecting" + name)
         }
         
       } catch {
@@ -34,7 +41,7 @@ object Collect {
       }
       
       
-    }
+    }*/
   
   }
 }
